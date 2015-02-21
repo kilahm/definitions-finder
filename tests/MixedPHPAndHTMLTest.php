3377ac33ac82fd271d9b6f9904d4698b515e1ed4
Fred Emmott <github@fredemmott.co.uk>
2015-02-21 07:22:40 +0000

    Test that mixing PHP and HTML doesnt confuse it

---
+++ tests/MixedPHPAndHTMLTest.php
@@ -0,0 +1,11 @@
+<?hh // strict
+
+class MixedPHPAndHTMLTest extends AbstractPHPTest {
+  protected function getFilename(): string {
+    return 'mixed_php_html.php';
+  }
+
+  protected function getPrefix(): string {
+    return '';
+  }
+}
