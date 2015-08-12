6d5638be7373b96ecbc9e4ef521ff36b647107be
Fred Emmott <fe@fb.com>
2015-08-12 13:32:28 -0700

    test and spuport whitespace between attribute values

--- src/consumers/UserAttributesConsumer.php
+++ src/consumers/UserAttributesConsumer.php
@@ -58,6 +58,7 @@ final class UserAttributesConsumer extends Consumer {
           'Expected attribute value to be followed by , or ) at line %d',
           $this->tq->getLine(),
         );
+        $this->consumeWhitespace();
       }
 
       list($t, $ttype) = $this->tq->shift();
