0ebb57c7bd488d0b7045c5dff8e39b69a75901b5
Fred Emmott <fe@fb.com>
2015-08-13 17:31:45 -0700

    Support concatenated strings in attributes

--- tests/AttributesTest.php
+++ tests/AttributesTest.php
@@ -7,7 +7,7 @@ use FredEmmott\DefinitionFinder\ScannedBase;
 use FredEmmott\DefinitionFinder\ScannedClass;
 use FredEmmott\DefinitionFinder\ScannedFunction;
 
-class ClassAttributesTest extends \PHPUnit_Framework_TestCase {
+class AttributesTest extends \PHPUnit_Framework_TestCase {
   private \ConstVector<ScannedClass> $classes = Vector {};
   private \ConstVector<ScannedFunction> $functions = Vector {};
 
@@ -92,6 +92,16 @@ class ClassAttributesTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals(
       Vector { Map { 'Bar' => Vector { } } },
       $params->map($x ==> $x->getAttributes()),
+    );
+  }
+
+  public function testConcatenatedValues(): void {
+    $data = "<?hh <<__Deprecated('herp'.'derp')>> function foo(){}";
+    $parser = FileParser::FromData($data);
+    $fun = $parser->getFunction('foo');
+    $this->assertEquals(
+      Map { '__Deprecated' => Vector { 'herpderp' } },
+      $fun->getAttributes(),
     );
   }
 
