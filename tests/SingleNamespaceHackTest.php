3db1b5fd35241c0d1259b97a6dfde72c9a2b839a
Fred Emmott <fred@fredemmott.co.uk>
2016-09-25 22:14:14 -0700

    Test that namespaced references are properly normalized
    fixes #27

--- tests/SingleNamespaceHackTest.php
+++ tests/SingleNamespaceHackTest.php
@@ -1,11 +1,35 @@
 <?hh // strict
 
-class SingleNamespaceHackTest extends AbstractHackTest {
+namespace FredEmmott\DefinitionFinder\Test;
+
+use FredEmmott\DefinitionFinder\FileParser;
+
+final class SingleNamespaceHackTest extends \AbstractHackTest {
   protected function getFilename(): string {
     return 'single_namespace_hack.php';
   }
 
   protected function getPrefix(): string {
     return 'SingleNamespace\\';
+  }
+
+  public function testConsistentNames(): void {
+    $data =
+      "<?hh\n".
+      "class Herp extends Foo\Bar {}\n".
+      "class Derp extends \Foo\Bar {}\n";
+
+    $parser = FileParser::FromData($data);
+    $herp = $parser->getClass('Herp');
+    $derp = $parser->getClass('Derp');
+
+    $this->assertSame(
+      'Foo\Bar',
+      $herp->getParentClassName(),
+    );
+    $this->assertSame(
+      $herp->getParentClassName(),
+      $derp->getParentClassName(),
+    );
   }
 }
